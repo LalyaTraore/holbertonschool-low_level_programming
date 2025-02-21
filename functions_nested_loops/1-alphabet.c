02559a6dae57d703d8d85a2600a399af65a60632
root <root@Lenovo-Lalya.>
2025-02-21 12:55:16 +0100

    1-alphabet.c

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* print_alphabet - print
+* main - entry
+* Return: 0
+*/
+
+void print_alphabet(void)
+{
+char a;
+for (a = 'a'; a <= 'z'; a++)
+_putchar(a);
+_putchar('\n');
+}
